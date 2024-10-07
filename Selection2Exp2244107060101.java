79fca0cd4ce08aba25025128d0d4726202b03223
Lovie-Tonimba <jechonialovie@gmail.com>
2024-10-07 20:05:42 +0700

    modification no1 experiment 2

--- Selection2Exp2244107060101.java
+++ Selection2Exp2244107060101.java
@@ -12,14 +12,28 @@ public class Selection2Exp2244107060101 {
         angle3 = input10.nextInt();
 
         totalAngle = angle1 + angle2 + angle3;
+        //Modification no1 experiment 2
         if(totalAngle == 180){
             if(angle1==90 || angle2==90 || angle3==90){
                 System.out.println("Right triangle");
+            }else if(angle1==60 && angle2==60 && angle3==60){
+                System.out.println("Equilateral triangle");
+            }else if(angle1==angle2 || angle1==angle3 || angle2==angle3){
+                System.out.println("Isosceles triangle");
             }else{
-                System.out.println("Not a right triangle");
+                System.out.println("Arbitrary triangle");
             }
-        }else{
+        }else {
             System.out.println("Not a triangle");
         }
-    }
+        // if(totalAngle == 180){
+        //     if(angle1==90 || angle2==90 || angle3==90){
+        //         System.out.println("Right triangle");
+        //     }else{
+        //         System.out.println("Not a right triangle");
+        //     }
+        // }else{
+        //     System.out.println("Not a triangle");
+        // }
+  }
 }
