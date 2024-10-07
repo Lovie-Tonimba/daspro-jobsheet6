b3c8398461eafbf0e7bafc9fc79ce097d8bf85c4
Lovie-Tonimba <jechonialovie@gmail.com>
2024-10-07 17:41:29 +0700

    Modifikasi no4 experiment 1

--- Selection2Exp1244107060101.java
+++ Selection2Exp1244107060101.java
@@ -11,14 +11,30 @@ public class Selection2Exp1244107060101 {
         System.out.print("Input year: ");
         year = input244107060101.nextInt();
 
-        if(year%4 == 0) {
-            if(year%100 != 0) {
-                System.out.println("Leap year");
+        //Modification no4 experiment 1
+        if(year%4 == 0){
+            if(year%100 == 0){
+                if(year%400 == 0){
+                    System.out.println("Leap year");
+                }else{
+                    System.out.println("Not a leap year");
+                }
             }else{
-                System.out.println("Not a leap year");
+                System.out.println("Leap year");
             }
         }else{
             System.out.println("Not a leap year");
         }
+
+        //Modification no2 experiment 1
+        // if(year%4 == 0) {
+        //     if(year%100 == 0) {
+        //         System.out.println("Leap year");
+        //     }else{
+        //         System.out.println("Not a leap year");
+        //     }
+        // }else{
+        //     System.out.println("Not a leap year");
+        // }
     }
 }
